a3a402098d0454b2128c049c13e48bcfdf93e790
Peter Fiorica <pnfioric@vortex-future.cbls.ccr.buffalo.edu>
2023-10-27 16:24:33 -0400

    corrected Gao PGS. Lifted over PGS to hg38 and recalculated PGS.

--- 03_call_pgs_calc.sh
+++ 03_call_pgs_calc.sh
@@ -2,15 +2,15 @@
 #SBATCH --reservation=ubhpc-future
 #SBATCH --partition=general-compute --qos=general-compute
 #SBATCH --time=12:00:00
-#SBATCH --job-name=PGS_calc_Shieh
+#SBATCH --job-name=PGS_calc_gao_erneg
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=16000
 ##SBATCH --requeue
-#SBATCH --output=pgs_job_6_Shieh.out
-#SBATCH --error=pgs_job_6_Shieh.err
+#SBATCH --output=pgs_job_4_gao_brca_neg.out
+#SBATCH --error=pgs_job_4_gao_brca_neg.err
 ###SBATCH --array=1,2,4,5,6
 
 module load gcc openmpi r-bundle-bioconductor
 
-Rscript UKB.PGS_PNF1.R 6
+Rscript UKB.PGS_PNF1.R 4
